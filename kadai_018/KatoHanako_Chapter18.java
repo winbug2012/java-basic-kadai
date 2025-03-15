61e9862b55780c1f3b9ee2a99b3a2a1be2f91d5f
winbug2012 <winbug2012@gmail.com>
2025-03-15 15:44:25 +0900

    Update KatoHanako_Chapter18.java

--- kadai_018/KatoHanako_Chapter18.java
+++ kadai_018/KatoHanako_Chapter18.java
@@ -3,10 +3,19 @@ package kadai_018;
 public class KatoHanako_Chapter18 extends Kato_Chapter18{
 
 	public KatoHanako_Chapter18() {
-		super("花子");
-	}
+        super();
+        setGivenName();
+}
+
+public void setGivenName() {
+	this.givenName="花子";
 	
-	public void eachIntroduce() {
-		System.out.println("趣味は読書です");
-	}
+}
+
+public void eachIntroduce() 
+{
+
+	System.out.println("趣味は読書です");
+}
+	
 }
