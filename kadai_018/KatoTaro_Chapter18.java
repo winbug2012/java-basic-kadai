539ceba4a00fae91fb119e1456d6d4570f85d3ab
winbug2012 <winbug2012@gmail.com>
2025-03-15 16:17:36 +0900

    Update KatoTaro_Chapter18.java

--- kadai_018/KatoTaro_Chapter18.java
+++ kadai_018/KatoTaro_Chapter18.java
@@ -2,12 +2,7 @@ package kadai_018;
 
 public class KatoTaro_Chapter18 extends Kato_Chapter18{
 	
-	
-	public KatoTaro_Chapter18() {	//コンストラクタ
-	        super();	//親クラスのコンストラクタを呼ぶ
-	        setGivenName();	//コンストラクタの中で普通のメソッドを呼ぶ
-	}
-	
+
 	public void setGivenName() {
 		this.givenName="太郎";
 		
