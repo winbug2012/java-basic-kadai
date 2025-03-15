58f7c680e57afb9c8ac93a9ec7d6208903eda171
winbug2012 <winbug2012@gmail.com>
2025-03-15 16:17:16 +0900

    Update Kato_Chapter18.java

--- kadai_018/Kato_Chapter18.java
+++ kadai_018/Kato_Chapter18.java
@@ -2,18 +2,14 @@ package kadai_018;
 
 public abstract class Kato_Chapter18 {
 	
-	public String familyName;
+	public String familyName="加藤";
 	public String givenName;
-	public String address;
+	public String address="東京都中野区〇×";
 	
-	Kato_Chapter18(){
-		this.familyName="加藤";
-		this.address="住所は東京都中野区〇×です";
-		
-	}
+
 	public void commonIntroduce() {
 		System.out.println("名前は"+familyName+givenName+"です");
-		System.out.println(address);
+		System.out.println("住所は"+address+"です");
 	};
 	
 	public abstract void eachIntroduce();
