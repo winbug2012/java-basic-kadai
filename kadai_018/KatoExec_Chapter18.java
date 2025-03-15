0d329b7af8af071d0576282a3cb9bcae1abdde7c
winbug2012 <winbug2012@gmail.com>
2025-03-15 15:44:50 +0900

    Update KatoExec_Chapter18.java

--- kadai_018/KatoExec_Chapter18.java
+++ kadai_018/KatoExec_Chapter18.java
@@ -4,22 +4,19 @@ public class KatoExec_Chapter18 {
 
 	public static void main(String[] args) {
 		// TODO 自動生成されたメソッド・スタブ
+	
 		KatoTaro_Chapter18 Taro = new KatoTaro_Chapter18();
 		KatoIchiro_Chapter18 Ichiro = new KatoIchiro_Chapter18();
 		KatoHanako_Chapter18 Hanako = new KatoHanako_Chapter18();
 		
-		
-		//Taro.commonIntroduce();
+		Taro.setGivenName();
 		Taro.execIntroduce();
-		Taro.eachIntroduce();
 		System.out.println("");
-		//Ichiro.commonIntroduce();
+		Ichiro.setGivenName();
 		Ichiro.execIntroduce();
-		Ichiro.eachIntroduce();
 		System.out.println("");
-		//Hanako.commonIntroduce();
+		Hanako.setGivenName();
 		Hanako.execIntroduce();
-		Hanako.eachIntroduce();
+	
 	}
-
 }
