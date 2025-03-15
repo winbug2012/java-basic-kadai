c15200bb48335216de8f6cd804065c893a6b13cf
winbug2012 <winbug2012@gmail.com>
2025-03-15 15:44:07 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -4,12 +4,18 @@ public class KatoIchiro_Chapter18 extends Kato_Chapter18{
 
 	
 	public KatoIchiro_Chapter18() {
-		super("一郎");
-	}
+        super();
+        setGivenName();
+}
+
+public void setGivenName() {
+	this.givenName="一郎";
 	
-	
-	
-	public void eachIntroduce() {
-		System.out.println("好きな食べ物はリンゴです");
-	}
+}
+
+public void eachIntroduce() 
+{
+
+	System.out.println("好きな食べ物はリンゴです");
+}
 }
