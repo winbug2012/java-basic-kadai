88d8812ff133fb91736f539d8f8d3a0fff298888
winbug2012 <winbug2012@gmail.com>
2025-03-15 16:17:46 +0900

    Update KatoIchiro_Chapter18.java

--- kadai_018/KatoIchiro_Chapter18.java
+++ kadai_018/KatoIchiro_Chapter18.java
@@ -2,11 +2,7 @@ package kadai_018;
 
 public class KatoIchiro_Chapter18 extends Kato_Chapter18{
 
-	
-	public KatoIchiro_Chapter18() {
-        super();
-        setGivenName();
-}
+
 
 public void setGivenName() {
 	this.givenName="一郎";
